4ce1070573e897dc651e2baabe4d7d906068308c
Tomwey <tomwey@163.com>
2014-03-30 00:21:12 +0800

    xxxx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/keke_official_website/current"
+root = "/home/deployer/apps/push_app/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn_keke_official_website.pid"
+pid "#{root}/tmp/pids/unicorn_push_app.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.keke_official_website.sock"
+listen "/tmp/unicorn.push_app.sock"
 worker_processes 2
 timeout 30
